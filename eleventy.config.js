4ed2617f72911753ff991fbb782d2a717f379c2f
ShadowPhoenix <github@shadowphoenix.nl>
2026-07-15 18:33:49 +0200

    Added graphs to use for creating contentGraph, navigationGraph and relationGraph incl documentation on how the relationGraph and its relationTypes work

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,8 +1,13 @@
 // Imports
+import fs from 'fs';
+import { rm } from 'fs/promises';
+import buildContentGraph from './lib/graphs/contentGraph.js';
+import buildNavigationGraph from './lib/graphs/navigationGraph.js';
+import buildRelationGraph from './lib/graphs/relationGraph.js';
 import markdown from './lib/markdown.config.js';
 import getNunjucksEnv from './lib/nunjucks.js';
 import registerShortcodes from './lib/shortcodes/index.js';
-import buildNavigationGraph from './lib/navigation.js';
+import relationTypes from './src/_data/relationTypes.js';
 
 /**
  * Eleventy Configuration
@@ -19,7 +24,12 @@ export default function (eleventy) {
     const layouts = "_layouts";
     const scripts = "_scripts";
     const styles = "_styles";
+    const theme = "base";
     const images = `${input}/img`;
+
+    let contentGraph;
+    let relationGraph;
+    let initialRun = true;
 
     // Nunjuck Environment
     const nunjucksEnv = getNunjucksEnv(`${workspace}/${components}`);
@@ -28,24 +38,69 @@ export default function (eleventy) {
     eleventy.setLibrary("md", markdown);
     eleventy.setLibrary("njk", nunjucksEnv);
 
-    // Creating Collection for Navigation
-    eleventy.addCollection("navGraph", function(collections) {
+    // Creating a ContentGraph
+    eleventy.addCollection("contentGraph", (collections) => {
         const pages = collections.all ?? collections.items;
 
         if (!pages) {
             console.warn("No pages found in collections!");
-            return { root: { name: "root", children: [], parent: null }, nodes: new Map(), flat: [] };
+            return { 
+                root: {
+                    name: "root",
+                    children: [],
+                    parent: null
+                },
+                nodes: new Map(),
+                flat: []
+            };
         }
 
-        console.log("Pages to build: ", pages.length);
-        return buildNavigationGraph(pages, input);
+        contentGraph = buildContentGraph(pages, input);
+        return contentGraph;
     });
+
+    // Creating a NavigationGraph based on ContentGraph
+    eleventy.addCollection("navGraph", () => {
+        return buildNavigationGraph(contentGraph);
+    });
+
+    // Enrich ContentGraph with Relations into RelationGraph
+    eleventy.addCollection("relationGraph", (collections) => {
+        relationGraph = buildRelationGraph(contentGraph, relationTypes);
+        return relationGraph;
+    })
+
+    // Filters
+    eleventy.addFilter("getRelations", (graph, stem) => {
+        const node = graph.getNodeByStem(stem);
+
+        const hasRelations = Object.keys(node.relations).length !== 0;
+        const hasBacklinks = Object.keys(node.backlinks).length !== 0;
+
+        if (hasRelations || hasBacklinks) return node;
+        else return null;
+    });
+
+    eleventy.addFilter("getAncestors", (graph, stem) => {
+        return graph.getActivePath(graph.getNodeByStem(stem))
+    });
+
+    eleventy.addFilter("getBreadcrumbs", (graph, stem) => {
+        return graph.getBreadcrumbs(graph.getNodeByStem(stem))
+    });
+
+    eleventy.addFilter("isArray", value => Array.isArray(value));
+
+    eleventy.addFilter("isObject", value => value !== null
+        && typeof value === "object"
+        && !Array.isArray(value)
+    );
 
     // Register (Paired) Shortcodes
     registerShortcodes(eleventy, nunjucksEnv, markdown);
 
     // Pass Through Copy
-    eleventy.addPassthroughCopy({ [`${workspace}/${styles}/`] : `${styles}` });
+    eleventy.addPassthroughCopy({ [`${workspace}/${styles}/${theme}`] : `${styles}` });
     eleventy.addPassthroughCopy({ [`${workspace}/${scripts}/`] : `${scripts}` });
     eleventy.addPassthroughCopy({ [`${workspace}/${images}/`] : "img" });
 
@@ -56,6 +111,22 @@ export default function (eleventy) {
 
     // Apply base.njk as default layout
     eleventy.addGlobalData("layout", "base");
+
+    eleventy.on("eleventy.before", async ({ dir, runMode }) => {
+        if (runMode === "build" || initialRun) await rm(dir.output, { recursive: true, force: true });
+        
+        initialRun = false;
+    })
+
+    eleventy.on("eleventy.after", async () => {
+        if (relationGraph.stubs.length) {
+            let stubArticles = `[RelationGraph] Stub articles (${relationGraph.stubs.length}):\r\n`;
+            relationGraph.stubs.foreach(s => stubArticles = stubArticles + `  - ${s.name}\r\n`);
+
+            console.log(stubArticles);
+            fs.writeFileSync('./stubArticles.txt', stubArticles);
+        }
+    })
 
     return {
         dir: {
